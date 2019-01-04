04dd824260ebb327b2c43f91b729fb7170d58b87
Adam Alayli <aadam35411@gmail.com>
2019-01-03 22:16:28 -0800

    Added histogram box

--- ui.r
+++ ui.r
@@ -2,7 +2,16 @@
 library(shiny)
 library(shinydashboard)
 
+sidebar <-dashboardSidebar(
+  sidebarMenu(
+    menuItem("Summary", tabName = "summary", icon = icon("book")),
+    menuItem("Graphs", icon = icon("chart-bar"), tabName = "graphs")
+  )
+)
+  
 ui <- dashboardPage(
+
+    
     dashboardHeader(title = "Student Grade Analyzer", titleWidth = 500),
     dashboardSidebar(width = 500),
     dashboardBody()
