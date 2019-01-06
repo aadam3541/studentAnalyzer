c5751ec46c94b341e4bdd032729a26d60861b576
Adam Alayli <aadam35411@gmail.com>
2019-01-06 12:42:31 -0800

    Added new tab

--- ui.r
+++ ui.r
@@ -15,8 +15,11 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(tabName = "summary",
-              box(title = "Histogram", status = "primary", plotOutput("plot2", height = 250))
-      )))
+              box(title = "Histogram", status = "primary", plotOutput("plot2", height = 250)),
+      tabItem(tabName = "graphs")
+      )
+      )
+    )
 )
 
 server <- function(input, output) { }
